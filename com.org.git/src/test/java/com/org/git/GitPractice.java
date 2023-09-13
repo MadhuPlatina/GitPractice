1f05cb5f0f0f4fc942b616875afcc359f3eaffc3
Venkata Bachu <venkata.bachu@gmail.com>
2023-09-12 20:40:42 -0400

    Added intial code

--- com.org.git/src/test/java/com/org/git/GitPractice.java
+++ com.org.git/src/test/java/com/org/git/GitPractice.java
@@ -1,8 +1,27 @@
 package com.org.git;
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class GitPractice {
 
-	public static void main(String[] args) {
+	static WebDriver driver;
+	public static void main(String[] args) throws Exception {
+		WebDriverManager.chromedriver().setup();
+		 driver=new ChromeDriver();
+		 driver.get("https://www.fooddepot.com/");
+		 String Title=driver.getTitle();
+		 driver.manage().window().maximize();
+		 driver.findElement(By.xpath("//ul[@id='top-menu']//a[text()='Weekly Ad']")).click();
+		 
+
+		
+		
 		
 	}
 
